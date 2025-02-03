ceefc7e814ababe9ab38b7976b3c3a39d91c1cd0
Your NameHasniUT01624 <wingscer@gmail.com>
2025-02-03 16:16:20 +0530

    method01

---
+++ Basic Proficiency (1–2 Months)/Week01_ Variables, Data Types, and Operators/Basic Questions (Variables and Data Types)/index.js
@@ -0,0 +1,29 @@
+//method01
+
+// Declare a variable name and assign your name to it. Print the value
+let Name;
+Name = "Sahaatha Hasni";
+console.log(Name);
+
+// Declare a variable age and assign your age to it. Print the value.
+let Age;
+Age = 26;
+console.log(Age);
+
+// Declare a variable isStudent and assign a boolean value (true or false) to it. Print the value.
+let isStudent;
+isStudent = true;
+console.log(isStudent);
+
+//Declare a variable price and assign a decimal number (e.g., 19.99) to it. Print the value.
+ let price;
+ price = 19.99;
+ console.log(price);
+
+//  Declare a variable city and assign the name of your city to it. Print the value.
+let city;
+city = "Oluvil";
+console.log(city);
+
+
+//method02
