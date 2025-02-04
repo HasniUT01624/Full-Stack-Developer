b97a91d5bc9cc8209fc12267ff218a8cea0a71cc
Your NameHasniUT01624 <wingscer@gmail.com>
2025-02-04 22:01:03 +0530

    5 operator question

---
+++ Basic Proficiency (1–2 Months)/Week01_ Variables, Data Types, and Operators/Operator-Based Questions/index.js
@@ -0,0 +1,22 @@
+// Declare two variables a and b and assign them numbers. Add them and print the result.
+let [a, b] = [34, 6];
+console.log(a + b);
+
+// Declare two variables x and y and assign them numbers. Subtract y from x and print the result.
+let [x, y] = [9, 45];
+console.log(y - x);
+
+// Declare two variables num1 and num2 and assign them numbers. Multiply them and print the result.
+let [num1, num2] = [2, 5];
+console.log(num1 * num2);
+
+// Declare two variables dividend and divisor and assign them numbers. Divide dividend by divisor and print the result.
+let [dividend, divisor] = [50, 2];
+console.log(dividend / divisor);
+
+// Declare two variables base and height and assign them numbers. Calculate the area of a triangle using the formula (base * height) / 2 and print the result.
+let [base, height] = [6, 78];
+let triangle = (base * height) / 2;
+console.log("this is area of triangle = " + triangle);
+
+
