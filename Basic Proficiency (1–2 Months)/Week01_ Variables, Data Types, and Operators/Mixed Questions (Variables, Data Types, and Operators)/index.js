a21112aa42fc24aac5016e3e161198fbb902c655
Your NameHasniUT01624 <wingscer@gmail.com>
2025-02-04 22:30:47 +0530

    05 question

---
+++ Basic Proficiency (1–2 Months)/Week01_ Variables, Data Types, and Operators/Mixed Questions (Variables, Data Types, and Operators)/index.js
@@ -0,0 +1,27 @@
+// Declare a variable radius and assign a number to it. Calculate the circumference of a circle using the formula 2 * π * radius and print the result. (Use π = 3.14159).
+let radius = 5;
+let pi = 3.14159;
+let circle = 2 * pi * radius;
+console.log("this is circumference of a circle " + circle);
+
+
+// Declar e a variable temperatureC and assign a temperature in Celsius. Convert it to Fahrenheit using the formula (temperatureC * 9/5) + 32 and print the result.
+let temperatureC = 32;
+let Fahrenheit = (temperatureC * 9 / 5) + 32;
+console.log(Fahrenheit);
+
+// Declare two variables length and width and assign them numbers. Calculate the area of a rectangle using the formula length * width and print the result.
+let [length, width] = [2, 4];
+let rectangle = [length * width];
+console.log(rectangle);
+
+// Declare a variable seconds and assign a number to it. Convert it to minutes using the formula seconds / 60 and print the result.
+// Declare a variable minutes and assign a number to it. Convert it to hours using the formula minutes / 60 and print the result.
+
+let seconds = 25;
+let minutes = seconds / 60;
+console.log(minutes);
+
+minutes = 30;
+const hours = minutes / 60;
+console.log(hours);
