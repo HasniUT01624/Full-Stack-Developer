89723ee9c4f22805766e62895ffaf01f3a944e13
Your NameHasniUT01624 <wingscer@gmail.com>
2025-02-08 00:43:03 +0530

    very easy qustions

---
+++ Basic Proficiency (1–2 Months)/Week01_ Variables, Data Types, and Operators/Real-Life Scenarios/index.js
@@ -0,0 +1,29 @@
+ // Declare a variable billAmount and assign a number to it. Calculate a 10% tip and print the total amount (billAmount + tip).
+let billAmount = 50;
+const percentage = 10;
+let tip = (percentage / 100) * billAmount;
+console.log("Total amount is = " + (billAmount + tip));
+
+
+// Declare a variable distance and assign a number (in kilometers). Convert it to meters using the formula distance * 1000 and print the result.
+let distance = 60;
+const meters = distance * 1000;
+console.log(meters + " km");
+
+// Declare a variable weight and assign a number (in kilograms). Convert it to grams using the formula weight * 1000 and print the result.
+let weight = 70;
+const grams = weight * 1000;
+console.log(grams + " g");
+
+// Declare a variable time and assign a number (in hours). Convert it to seconds using the formula time * 3600 and print the result.
+let time = 2;
+const minutes = time * 60;
+console.log(minutes + " minutes");
+const seconds = minutes * 60;
+console.log(seconds + " sec");
+
+
+// Declare a variable salary and assign a number. Calculate the annual salary by multiplying it by 12 and print the result.
+let salary = 45000;
+const annual = salary * 12;
+console.log( "if you earn " +salary+ " your annual ammount is = "+annual);
